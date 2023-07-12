a026493127f8c4230cd84f7a95dad02a8cdef11b
The-Zealot <96833407+The-Zealot@users.noreply.github.com>
2023-07-12 12:58:04 +0700

    Realized of base displaying of linkWidget

--- GuiLib/linkwidget.cpp
+++ GuiLib/linkwidget.cpp
@@ -2,5 +2,42 @@
 
 LinkWidget::LinkWidget(QWidget *parent) : QWidget(parent)
 {
+    _layout     = new QVBoxLayout(this);
+    _button     = new QPushButton(this);
+    _labelName  = new QLabel(this);
 
+    this->setLayout(_layout);
+
+    _labelName->setAlignment(Qt::AlignCenter);
+    _button->setFlat(true);
+    _button->setIconSize(QSize(64, 64));
+
+    this->resize(90, 90);
+
+    _layout->addWidget(_button, 0);
+    _layout->addWidget(_labelName, 1);
+    _layout->setSpacing(0);
+
+    _button->setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding));
+    _labelName->setSizePolicy(QSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed));
+
+    _labelName->show();
+    _button->show();
+
+    _type = Empty;
+}
+
+void LinkWidget::setLabel(const QString &name)
+{
+    _labelName->setText(name);
+}
+
+void LinkWidget::setLink(const QString &path)
+{
+    _path = path;
+}
+
+void LinkWidget::setIcon(const QIcon &icon)
+{
+    _button->setIcon(icon);
 }
--- GuiLib/linkwidget.h
+++ GuiLib/linkwidget.h
@@ -2,17 +2,43 @@
 #define LINKWIDGET_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
 
 #include "GuiLib_global.h"
 
 class GUILIB_EXPORT LinkWidget : public QWidget
 {
     Q_OBJECT
+
+    enum LinkType
+    {
+        Empty = 0,
+        Folder,
+        File,
+    };
+
 public:
     explicit LinkWidget(QWidget *parent = nullptr);
 
+    void setLabel(const QString &name);
+    void setLink(const QString &path);
+    void setIcon(const QIcon &icon);
+
+    QString getLabel() const;
+    QString getLink() const;
+
 signals:
 
+private:
+    QPushButton *_button;
+    QLabel *_labelName;
+    QVBoxLayout *_layout;
+
+    QString _path;
+
+    LinkType _type;
 };
 
 #endif // LINKWIDGET_H
